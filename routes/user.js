8c8249080bc8c959a75f3c8961627c6c1f592c81
Ruth Tshoms <tshomsruth@gmail.com>
2022-11-28 17:54:44 +0100

    fonction login dans user/controllers

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/user');
+//const User = require('../models/user');
 
 const userCtrl = require('../controllers/user');
 
 
 
-router.post('/login', userCtrl.login);
 router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
 
 module.exports = router;
