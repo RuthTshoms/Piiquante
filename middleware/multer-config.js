5505ad307da03adcd4951547de81642c573ec3d3
Ruth Tshoms <tshomsruth@gmail.com>
2022-12-01 15:40:32 +0100

    ajout commentaires

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,18 +1,22 @@
+//** Configuration de multer, gestionnaire de fichiers entrants */
+
 const multer = require('multer');
 
+// Dictionnaire d'extensions d'images //
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
+// Configuration de multer //
+const storage = multer.diskStorage({ 
+  destination: (req, file, callback) => { // où enregistrer les fichiers
+    callback(null, 'images'); // (pas d'erreur, nom du dossier)
   },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
+  filename: (req, file, callback) => { // quel nom de fichier utiliser
+    const name = file.originalname.split(' ').join('_'); // supp des espaces et ajout de _ sur le nom d'origine du fichier 
+    const extension = MIME_TYPES[file.mimetype]; // application d'une extension au fichier 
     callback(null, name + Date.now() + '.' + extension);
   }
 });
