fcd66fcc62b5028355b4ef893e4c147129eac44d
Ruth Tshoms <tshomsruth@gmail.com>
2022-12-21 17:24:50 +0100

    commentaires

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const express = require('express'); // framework qui permet le déploiement de l'API plus rapidement
+const router = express.Router(); // fonction qui permet de créer un nouvel objet Router 
+const auth = require('../middleware/auth'); // importation du middleware qui permet l'authentification d'un utilisateur 
+const multer = require('../middleware/multer-config'); // importation du middleware qui gère les fichiers entrants 
 
 const sauceCtrl = require('../controllers/sauce'); 
 
+//** Préciser la fonction de chaque route */
+//** Lister les routes disponibles à quelles points de terminaisons et pour quelles fonctions et controllers  */
 router.get('/', auth, sauceCtrl.getAllSauces); 
 router.post('/', auth, multer, sauceCtrl.createSauce); 
 //router.get('/:id', sauceCtrl.getOneSauce);
--- routes/user.js
+++ routes/user.js
@@ -10,3 +10,5 @@ router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
+
+// voir commentaire sur router/sauce
