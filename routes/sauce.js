e20a5c53b3f94cb0a29b74da86ee839c6bbdae40
Ruth Tshoms <tshomsruth@gmail.com>
2022-12-22 15:52:02 +0100

    fonction pour supprimer une sauce ok

--- routes/sauce.js
+++ routes/sauce.js
@@ -11,6 +11,8 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce); 
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
 
 
 module.exports = router;
