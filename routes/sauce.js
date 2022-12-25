8f08748e20e06f3895ed46260dceb1d76e39c0d3
Ruth Tshoms <tshomsruth@gmail.com>
2022-12-25 16:15:29 +0100

    fonction pour liker, disliker, supprimer son like ou dislike ok

--- routes/sauce.js
+++ routes/sauce.js
@@ -4,6 +4,7 @@ const auth = require('../middleware/auth'); // importation du middleware qui per
 const multer = require('../middleware/multer-config'); // importation du middleware qui gère les fichiers entrants 
 
 const sauceCtrl = require('../controllers/sauce'); 
+const sauce = require('../models/sauce');
 
 //** Préciser la fonction de chaque route */
 //** Lister les routes disponibles à quelles points de terminaisons et pour quelles fonctions et controllers  */
@@ -12,6 +13,7 @@ router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.like);
 
 
 
